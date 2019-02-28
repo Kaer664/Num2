d480603a294e1607f38bc157154d7dc2d106a229
lucky <632496308@qq.com>
2019-02-28 19:09:30 +0800

    cai  本地加载完毕  准备合并guo

---
+++ app/src/main/java/bean/BusStationIdBean.java
@@ -0,0 +1,32 @@
+package bean;
+
+/**
+ * Created by lucky on 2019/2/28.
+ */
+
+public class BusStationIdBean {
+
+    /**
+     * Distance : 50800
+     * BusId : 1
+     */
+
+    private int Distance;
+    private int BusId;
+
+    public int getDistance() {
+        return Distance;
+    }
+
+    public void setDistance(int Distance) {
+        this.Distance = Distance;
+    }
+
+    public int getBusId() {
+        return BusId;
+    }
+
+    public void setBusId(int BusId) {
+        this.BusId = BusId;
+    }
+}
--- app/src/main/java/edevelopmentandroidcompanydomain/myapplication/MainActivity.java
+++ app/src/main/java/edevelopmentandroidcompanydomain/myapplication/MainActivity.java
@@ -12,12 +12,17 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import bean.AllSenseBean;
+import bean.BusStationIdBean;
 import util.Address;
 import util.HttpUtil;
 
@@ -40,6 +45,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private static final int HANDLER_SUCCESS_INT=0x001;
     private static final int HANDLER_ERROR_INT=0x002;
+
+    //这个变量用于startThread方法中控制线程，线程运行标识符
+    private Boolean sign=false;
+
+    /**
+     * start方法用于开启线程
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sign=true;
+    }
+
+    /**
+     * onStop用于应用退出关闭线程
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sign=false;
+    }
     /**
      * 此方法用于调用线程在给得到网络数据
      */
@@ -47,29 +73,97 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         new Thread(){
             @Override
             public void run() {
-                try {
-                    String sense=HttpUtil.postJson(Address.GetAllSense,"","");
-                    Message msg=handler.obtainMessage(HANDLER_SUCCESS_INT);
-                    handler.sendMessage(msg);
-                } catch (IOException e) {
-
-                }
+              while (sign){
+                  Log.i("TestNum",String.valueOf(sign));
+                  try {
+                      Map<String,String> info=new HashMap<>();
+                      //得到环境信息
+                      String sense=HttpUtil.postJson(Address.GetAllSense,"","");
+                      //发送公家车站台信息
+                      String station1=HttpUtil.postJson(Address.GetBusStationInfo,"BusStationId","1");
+                      String station2=HttpUtil.postJson(Address.GetBusStationInfo,"BusStationId","2");
+                      Message msg=handler.obtainMessage(HANDLER_SUCCESS_INT);
+                      info.put("sense",sense);
+                      info.put("station1",station1);
+                      info.put("station2",station2);
+                      msg.obj=info;
+                      handler.sendMessage(msg);
+                  } catch (IOException e) {
+                      Log.i("TestNum","返回错误 请检查权限或者网络状态");
+                  }
+              }
+                Log.i("TestNum",String.valueOf(sign));
             }
         }.start();
     }
+    private Gson gson=new Gson();
+
 
     private Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case HANDLER_SUCCESS_INT:
-                    Log.i("TestNum","连接成功  下面可以进行下一步计划");
+                    Map <String,String> info= (Map<String, String>) msg.obj;
+                    String sense=info.get("sense");
+                    String station1=info.get("station1");
+                    String station2=info.get("station2");
+                    Map<String,String> map=parserSense(sense);  //方法解析完数据 返回map数据
+                    tvPMValue.setText(map.get("pmValue"));
+                    tvCO2Value.setText(map.get("co2Value"));
+                    tvHumidityValue.setText(map.get("humidityValue"));
+                    tvTemperatureValue.setText(map.get("temperatureValue"));
+                    List<String> distance1=parserStation(station1);
+                    List<String> distance2=parserStation(station2);
+                    tvNum1Bus1.setText(distance1.get(0));
+                    tvNum1Bus2.setText(distance1.get(1));
+                    tvNum2Bus1.setText(distance2.get(0));
+                    tvNum2Bus2.setText(distance2.get(1));
                     break;
                 case HANDLER_ERROR_INT:
                     break;
             };
         }
     };
+
+    /**
+     *
+     * @param station1  解析的数据
+     * @return   返回为list  数据
+     */
+    private List<String> parserStation(String station1){
+        List<BusStationIdBean> busStationIdBeans = gson.fromJson(station1,
+                new TypeToken<List<BusStationIdBean>>() {
+                }.getType());
+        List<String> data=new ArrayList<>();
+        for(int i=0;i<busStationIdBeans.size();i++){
+            int busID=busStationIdBeans.get(i).getBusId();
+            int distance=busStationIdBeans.get(i).getDistance();
+            data.add(String.valueOf(distance));
+        }
+        return data;
+    }
+    /**
+     * 方法主要用与解析出sense的值，然后通过Map返回出来
+     * @param sense  传入的json值
+     * @return  返回Map值
+     */
+    private Map<String, String> parserSense(String sense){
+        AllSenseBean allSense=gson.fromJson(sense, AllSenseBean.class);
+        AllSenseBean.ServerInfoBean infoBean=allSense.getServerInfo();
+        String pmValue=String.valueOf(infoBean.get_$Pm25254());
+        String co2Value=String.valueOf(infoBean.getCo2());
+        String humidityValue=String.valueOf(infoBean.getHumidity());
+        String lightIntensityValue=String.valueOf(infoBean.getLightIntensity());
+        String temperatureValue=String.valueOf(infoBean.getTemperature());
+        Map<String,String> map=new HashMap<>();
+        map.put("pmValue",pmValue);
+        map.put("co2Value",co2Value);
+        map.put("humidityValue",humidityValue);
+        map.put("lightIntensityValue",lightIntensityValue);
+        map.put("temperatureValue",temperatureValue);
+        return map;
+    }
 
     private void init() {
         tvNum1Bus1=findViewById(R.id.tvNum1Bus1);
